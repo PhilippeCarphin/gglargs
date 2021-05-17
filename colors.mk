fc151078df16e9898fe4046e0a45a0dfa16b7e9f
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-05-17 14:58:24 -0400

    Added conditional for echo

--- colors.mk
+++ colors.mk
@@ -1,35 +1,41 @@
+UNAME := $(shell uname)
+ifeq ($(UNAME),Linux)
+	ECHO := echo -e
+else
+	ECHO := echo
+endif
 
 define make_echo_color_bold
 	@case $(1) in \
-		red)     echo "\033[1;31m$(2)\033[0m" ;; \
-		green)   echo "\033[1;32m$(2)\033[0m" ;; \
-		yellow)  echo "\033[1;33m$(2)\033[0m" ;; \
-		blue)    echo "\033[1;34m$(2)\033[0m" ;; \
-		magenta) echo "\033[1;35m$(2)\033[0m" ;; \
-		cyan)    echo "\033[1;36m$(2)\033[0m" ;; \
-		white)   echo "\033[1;37m$(2)\033[0m" ;; \
-		*)       echo "\033[1;4m$(2)\033[0m"  ;; \
+		red)     $(ECHO) "\033[1;31m$(2)\033[0m" ;; \
+		green)   $(ECHO) "\033[1;32m$(2)\033[0m" ;; \
+		yellow)  $(ECHO) "\033[1;33m$(2)\033[0m" ;; \
+		blue)    $(ECHO) "\033[1;34m$(2)\033[0m" ;; \
+		magenta) $(ECHO) "\033[1;35m$(2)\033[0m" ;; \
+		cyan)    $(ECHO) "\033[1;36m$(2)\033[0m" ;; \
+		white)   $(ECHO) "\033[1;37m$(2)\033[0m" ;; \
+		*)       $(ECHO) "\033[1;4m$(2)\033[0m"  ;; \
 	esac
 endef
 
 define make_echo_color
 	@case $(1) in \
-		red)     echo "\033[31m$(2)\033[0m" ;; \
-		green)   echo "\033[32m$(2)\033[0m" ;; \
-		yellow)  echo "\033[33m$(2)\033[0m" ;; \
-		blue)    echo "\033[34m$(2)\033[0m" ;; \
-		magenta) echo "\033[35m$(2)\033[0m" ;; \
-		cyan)    echo "\033[36m$(2)\033[0m" ;; \
-		white)   echo "\033[37m$(2)\033[0m" ;; \
-		*)       echo "\033[4m$(2)\033[0m"  ;; \
+		red)     $(ECHO) "\033[31m$(2)\033[0m" ;; \
+		green)   $(ECHO) "\033[32m$(2)\033[0m" ;; \
+		yellow)  $(ECHO) "\033[33m$(2)\033[0m" ;; \
+		blue)    $(ECHO) "\033[34m$(2)\033[0m" ;; \
+		magenta) $(ECHO) "\033[35m$(2)\033[0m" ;; \
+		cyan)    $(ECHO) "\033[36m$(2)\033[0m" ;; \
+		white)   $(ECHO) "\033[37m$(2)\033[0m" ;; \
+		*)       $(ECHO) "\033[4m$(2)\033[0m"  ;; \
 	esac
 endef
 
 define big_success
-	@echo "\033[1;32m✓\033[0m"
+	@$(ECHO) "\033[1;32m✓\033[0m"
 endef
 define success
-	@echo "\033[32m✓\033[0m"
+	@$(ECHO) "\033[32m✓\033[0m"
 endef
 define make_echo_build_c_object
 	$(call make_echo_color,green,"Building C object $@")
